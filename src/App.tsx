98e15d7653b13ed479aa2bc3ebbd367dbe94dfbc
Muhammed Shaheer <ckshaheer2012@gmail.com>
2022-02-25 09:18:31 +0530

    List rendered with typing jsx element array

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,19 @@ interface IState {
 }
 
 function App() {
-    const [people, setPeople] = useState<IState["people"]>([]);
+    const [people, setPeople] = useState<IState["people"]>([
+        {
+            name: "LeBron James",
+            url: "https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png",
+            age: 36,
+            note: "Allergic to stay on the same team"
+        },
+        {
+            name: "Kobe Bryant",
+            url: "https://fullpresscoverage.com/wp-content/uploads/2020/01/101524695-457220551.jpg",
+            age: 42
+        }
+    ]);
 
     return (
         <div className="App">
